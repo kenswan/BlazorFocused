2f5cb47590236f0d26d42a75f59266cad271fb25
Kenneth Swan <kswan@tiercore.com>
2020-08-05 22:44:47 -0500

    Builder WIP

---
+++ src/BlazorRx.Core/Builder/IStoreBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BlazorRx.Core.Reducer;
+
+namespace BlazorRx.Core.Builder
+{
+    public interface IStoreBuilder<TState>
+    {
+        IStoreBuilder<TState> RegisterAction(Type type);
+        IStoreBuilder<TState> RegisterReducer(Type type);
+    }
+}
---
+++ src/BlazorRx.Core/Builder/StoreBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BlazorRx.Core.Builder
+{
+    public class StoreBuilder<TState> : IStoreBuilder<TState> where TState : class
+    {
+        public IStoreBuilder<TState> RegisterAction(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStoreBuilder<TState> RegisterReducer(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
