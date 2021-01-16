7b631502b60448628c9fdb65a901eaeea4546c24
Kenneth Swan <kswan@tiercore.com>
2021-01-16 16:48:59 -0600

    fix client registration

--- src/Client/ServiceCollectionExtensions.cs
+++ src/Client/ServiceCollectionExtensions.cs
@@ -6,18 +6,23 @@ namespace BlazorFocused.Client
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddRestClient(this IServiceCollection services, string baseUrl)
-        {
+        public static void AddRestClient(this IServiceCollection services, string baseUrl) =>
             services.AddHttpClient<IRestClient, RestClient>(client =>
                 client.BaseAddress = new Uri(baseUrl));
-        }
 
-        public static void AddRestClient(this IServiceCollection services, Action<HttpClient> configureClient)
+        public static void AddRestClient(
+            this IServiceCollection services,
+            Action<HttpClient> configureClient = null)
         {
-            if (configureClient != null)
+            if (configureClient is null)
+            {
+                services.AddHttpClient<IRestClient, RestClient>();
+            }
+            else
             {
                 services.AddHttpClient<IRestClient, RestClient>(configureClient);
             }
+
         }
     }
 }
