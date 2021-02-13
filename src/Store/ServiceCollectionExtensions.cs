cb31ae1ff9629734ce798cc8cf80b795b0aaeb26
kenswan <kswan@tiercore.com>
2021-02-12 18:32:11 -0600

    Initial Http Test Framework

--- src/Store/ServiceCollectionExtensions.cs
+++ src/Store/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using BlazorFocused.Client;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorFocused.Store
@@ -7,23 +6,23 @@ namespace BlazorFocused.Store
     public static class ServiceCollectionExtensions
     {
         public static void AddStore<T>(
+            this IServiceCollection services, T initialData) where T : class
+        {
+            services.AddScoped<IStore<T>, Store<T>>(serviceProvider =>
+                new Store<T>(initialData));
+        }
+
+        public static void AddStore<T>(
             this IServiceCollection services,
             T initialData,
-            Action<IStoreBuilder<T>> builderFunction = null) where T : class
+            Action<IStoreBuilder<T>> builderFunction) where T : class
         {
             IStoreBuilder<T> builder = new StoreBuilder<T>();
 
-            if (builderFunction is not null)
-            {
-                builderFunction(builder);
-            }
+            builderFunction(builder);
 
             services.AddScoped<IStore<T>, Store<T>>(serviceProvider =>
-            {
-                var store = new Store<T>(initialData, builder);
-
-                return store;
-            });
+                new Store<T>(initialData, builder));
         }
     }
 }
---
+++ test/Testing/FocusedHttpTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BlazorFocused.Core.Test.Model;
+using Bogus;
+using FluentAssertions;
+using Xunit;
+
+namespace BlazorFocused.Testing.Test
+{
+    public class FocusedHttpTests
+    {
+        private readonly FocusedHttp focusedHttp;
+        private readonly string baseAddress;
+
+        public FocusedHttpTests()
+        {
+            baseAddress = new Faker().Internet.Url();
+            focusedHttp = new FocusedHttp(baseAddress);
+        }
+
+        [Fact]
+        public void ShouldSetBaseUri()
+        {
+            var client = focusedHttp.Client();
+
+            Assert.Equal(baseAddress, client.BaseAddress.OriginalString);
+        }
+
+        [Fact]
+        public async Task ShouldReturnSetResponse()
+        {
+            var requestUrl = "api/test";
+            var expectedReponseCode = HttpStatusCode.OK;
+            var expectedResponseObject =
+                new SimpleClass { FieldOne = "Test1", FieldTwo = "Test2", FieldThree = "Field3" };
+
+            focusedHttp.Setup(request =>
+            {
+                request.HttpMethod = HttpMethod.Get;
+                request.Url = requestUrl;
+                return request;
+            }, expectedReponseCode, expectedResponseObject);
+
+            var client = focusedHttp.Client();
+            var actualResponse = await client.GetAsync(requestUrl);
+            var actualStatusCode = actualResponse?.StatusCode;
+            var actualResponseString = await actualResponse.Content.ReadAsStringAsync();
+            var actualResponseObject = JsonSerializer.Deserialize<SimpleClass>(actualResponseString);
+
+            Assert.Equal(expectedReponseCode, actualResponse.StatusCode);
+            actualResponseObject.Should().BeEquivalentTo(expectedResponseObject);
+        }
+
+        [Fact]
+        public async Task ShouldVerifyRequestMade()
+        {
+            var requestUrl = "api/test/4";
+            var expectedReponseCode = HttpStatusCode.OK;
+            var expectedResponseObject =
+                new SimpleClass { FieldOne = "Test1", FieldTwo = "Test2", FieldThree = "Field3" };
+
+            focusedHttp.Setup(request =>
+            {
+                request.HttpMethod = HttpMethod.Get;
+                request.Url = requestUrl;
+                return request;
+            }, expectedReponseCode, expectedResponseObject);
+
+            var client = focusedHttp.Client();
+            await client.GetAsync(requestUrl);
+
+            var calledException = Record.Exception(() => focusedHttp.VerifyWasCalled());
+
+            var calledWithMethodException =
+                Record.Exception(() => focusedHttp.VerifyWasCalled(HttpMethod.Get));
+
+            var calledWithMethodAndUrlException =
+                Record.Exception(() => focusedHttp.VerifyWasCalled(HttpMethod.Get, requestUrl));
+
+            Assert.Null(calledException);
+            Assert.Null(calledWithMethodException);
+            Assert.Null(calledWithMethodAndUrlException);
+        }
+
+        [Fact]
+        public void ShouldVerifyRequestNotMade()
+        {
+            var requestUrl = "api/test/4";
+            var expectedReponseCode = HttpStatusCode.OK;
+            var expectedResponseObject =
+                new SimpleClass { FieldOne = "Test1", FieldTwo = "Test2", FieldThree = "Field3" };
+
+            focusedHttp.Setup(request =>
+            {
+                request.HttpMethod = HttpMethod.Post;
+                request.Url = requestUrl;
+                return request;
+            }, expectedReponseCode, expectedResponseObject);
+
+            Action act = () => focusedHttp.VerifyWasCalled();
+            Action actWithMethod = () => focusedHttp.VerifyWasCalled(HttpMethod.Get);
+            Action actWithMethodAndUrl = () => focusedHttp.VerifyWasCalled(HttpMethod.Get, requestUrl);
+
+            act.Should().Throw<FocusedTestException>();
+
+            actWithMethod.Should().Throw<FocusedTestException>()
+                .Where(exception => exception.Message.Contains(HttpMethod.Get.ToString()));
+
+            actWithMethodAndUrl.Should().Throw<FocusedTestException>()
+                .Where(exception => exception.Message.Contains(HttpMethod.Get.ToString()) &&
+                    exception.Message.Contains(requestUrl));
+        }
+    }
+}
