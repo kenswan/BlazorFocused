0c9c1796546921f61476f0b1193f7c614725f153
Ken Swan <kswan@tiercore.com>
2021-08-26 17:58:06 -0500

    ToDo List Sample

---
+++ samples/ToDoList/Client/Stores/ToDoStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ToDoList.Shared;
+
+namespace ToDoList.Client.Stores
+{
+    public class ToDoStore
+    {
+        public List<ToDo> InComplete { get; set; } = new();
+
+        public List<ToDo> Complete { get; set; } = new();
+    }
+}
---
+++ samples/ToDoList/Shared/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Shared
+{
+    public class ToDo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
